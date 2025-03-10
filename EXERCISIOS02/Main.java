dbf93a1c1c365d0a8025298646fcc0e869c1abfe
joseEduardoDS08 <je3751907@gmail.com>
2025-03-10 10:13:51 -0300

    Add files via upload

---
+++ EXERCISIOS02/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void Main(String[] args){
+        scan
+    }
+    switch (menu) {
+  case 1:
+    System.out.println("Cadastrar Pessoa: ");
+    break;
+  case 2:
+    System.out.println("Exibir Pessoa: ");
+    break;
+  case 3:
+    System.out.println("Editar Pessoa: ");
+    break;
+  case 4:
+    System.out.println("Excluir Pessoa: ");
+    break;
+  case 5:
+    System.out.println("Sair");
+    break;
+    default:
+        System.out.println("-------------------------");
+    }
+}
---
+++ EXERCISIOS02/Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa{
+    String nome;
+    int idade;
+    String Email;
+    
+    public void setNome(String nome) {
+    this.nome = nome;
+    }
+    public void setIdade(int idade) {
+    this.idade = idade;
+    }
+    public void setEmail(String Email) {
+    this.Email = Email;
+    }
+    public Pessoa(String nome, int idade, String Email) {
+        nome = nome;
+        idade = idade;
+        Email = Email;
+    }
+    public static void Pessoa(String[] args) {
+        
+    }
+
+}
